5232d775c7a9a8749f2aa04b938d63057a16e485
RamosDev <matheus15910@gmail.com>
2022-03-27 22:13:50 -0300

    Estudo sobre map e set em javascript e projeto javascript assíncrono com integrãção de API

---
+++ Estudos MAP e SET em Javascript/map&set.js
@@ -0,0 +1,25 @@
+const myMap = new Map();
+
+myMap.set ('Apple', 'Fruit'); // seta primeira a chave e depois o valor
+
+myMap.get('Apple');
+
+myMap.delete ("Apple");
+
+// maps podem ter chaves de qualquer tipo
+
+//maps possuem propriedade length
+
+// maps são fáceis de iterar
+
+// utilizando quando não conhecemos o valor da chave
+
+// os valores tem o mesmo tipo
+
+const mySet = new Set();
+
+mySet.add (1); // quando quer adicionar um valor
+
+mySet.has (1); // quando quer verificar se existe um valores
+
+mySet.delete(1); //deletar o valor existente 
